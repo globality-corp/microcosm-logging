a912317e658372eebc66d6ff70a06ed091327236
Adam Ever Hadani <adamhadani@gmail.com>
2016-04-19 11:37:45 -0700

    added requests[security] dependency

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
     install_requires=[
         "loggly-python-handler>=1.0.0",
         "microcosm>=0.3.0",
+        "requests[security]>=2.9.1",
     ],
     setup_requires=[
         "nose>=1.3.6",
