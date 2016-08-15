5d1ec7cc663f18574b27a6213a8a8c14ed8b6d5b
cdm-ium <constantine.dm@gmail.com>
2016-08-15 11:07:30 -0700

    adding context logger

--- microcosm_logging/decorators.py
+++ microcosm_logging/decorators.py
@@ -1,5 +1,5 @@
 """Decorator library for common logging functionality."""
-from logging import getLogger
+from logging import getLogger, LoggerAdapter
 
 
 def logger(obj):
@@ -15,3 +15,54 @@ def logger(obj):
     """
     obj.logger = getLogger(obj.__name__)
     return obj
+
+
+class ContextLogger(LoggerAdapter):
+    """
+    Allows for inserting additional context into log records based on the current context.
+    To play nicely with the pythonjsonlogger used for json logs, a ContextLogger will
+    update keys on the existing message object if the message is a dictionary otherwise,
+    it will prepend context information before the log message in brackets.
+
+    """
+    def process(self, msg, kwargs):
+        if isinstance(msg, dict):
+            msg.update(self.extra)
+        else:
+            headers_string = [
+                '{header_name}: {header_value}'.format(
+                    header_name=header_name,
+                    header_value=header_value
+                ) for header_name, header_value in self.extra.iteritems()
+            ]
+            msg = '{} {}'.format(
+                "[{}]".format(" ,".join(headers_string)),
+                msg
+            )
+        return msg, kwargs
+
+
+def context_logger(context_func, func, parent=None):
+    """
+    The results of context_func will be executed and applied to a ContextLogger
+    instance for the execution of func. The resulting ContextLogger instance will be
+    available on parent.logger for the duration of func.
+
+    :param context_func: callable which provides dictionary-like context information
+    :param func: the function to wrap
+    :param parent: object to attach the context logger to, if None, defaults to func.__self__
+    """
+    if parent is None:
+        parent = func.__self__
+
+    def wrapped(*args, **kwargs):
+        parent.logger = ContextLogger(
+            getattr(parent, 'logger', getLogger(parent.__class__.__name__)),
+            context_func(*args, **kwargs),
+        )
+        try:
+            result = func(*args, **kwargs)
+            return result
+        finally:
+            parent.logger = parent.logger.logger
+    return wrapped
--- microcosm_logging/tests/test_decorators.py
+++ microcosm_logging/tests/test_decorators.py
@@ -2,11 +2,15 @@ from logging import getLogger
 
 from hamcrest import (
     assert_that,
+    calling,
     equal_to,
     is_,
+    raises,
 )
 
-from microcosm_logging.decorators import logger
+from mock import Mock
+
+from microcosm_logging.decorators import logger, context_logger
 
 
 @logger
@@ -15,7 +19,43 @@ class TestClass(object):
     pass
 
 
+@logger
+class TestContextClass(object):
+
+    def function(self):
+        self.logger.info("success!")
+
+
 def test_using_class_logger_works():
     instance = TestClass()
 
     assert_that(instance.logger, is_(equal_to(getLogger(instance.__class__.__name__))))
+
+
+def test_using_context_logger_works():
+    instance = TestContextClass()
+    context_func = Mock(return_value={'some': 'context'})
+    wrapped = context_logger(context_func, instance.function, instance)
+
+    wrapped()
+    context_func.assert_called_once()
+
+
+def test_using_context_logger_no_parent():
+    instance = TestContextClass()
+    context_func = Mock(return_value={'some': 'context'})
+    wrapped = context_logger(context_func, instance.function)
+
+    wrapped()
+    context_func.assert_called_once()
+
+
+def test_using_context_logger_no_parent_no_class_fails():
+    def function():
+        pass
+
+    context_func = Mock(return_value={'some': 'context'})
+    assert_that(
+        calling(context_logger).with_args(context_func, function),
+        raises(AttributeError),
+    )
