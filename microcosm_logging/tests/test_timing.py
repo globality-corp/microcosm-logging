4c86013643060e407766f46184bcc7408afb3346
Jesse Myers <jesse.myers@globality.com>
2016-10-13 10:51:01 -0700

    Add timing support

---
+++ microcosm_logging/tests/test_timing.py
@@ -0,0 +1,20 @@
+"""
+Timing usage tests.
+
+"""
+from time import sleep
+
+from hamcrest import (
+    assert_that,
+    close_to,
+    is_,
+)
+from microcosm_logging.timing import elapsed_time
+
+
+def test_elapsed_time():
+    target = dict()
+    with elapsed_time(target):
+        sleep(0.1)
+
+    assert_that(target["elapsed_time"], is_(close_to(0.1, 0.01)))
