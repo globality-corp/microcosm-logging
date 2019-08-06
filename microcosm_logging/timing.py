6a196d93120175f685ae107b5b542d9c44b8a62c
Salman Mashayekh <salman.mashayekh@globality.com>
2019-08-06 11:18:49 -0700

    replace `time.time()` with `time.perf_counter()` for timing

--- microcosm_logging/timing.py
+++ microcosm_logging/timing.py
@@ -3,14 +3,14 @@ Simple timer support for logging elapsed time.
 
 """
 from contextlib import contextmanager
-from time import time
+from time import perf_counter
 
 
 @contextmanager
 def elapsed_time(target):
-    start_time = time()
+    start_time = perf_counter()
     try:
         yield start_time
     finally:
-        elapsed_ms = (time() - start_time) * 1000
+        elapsed_ms = (perf_counter() - start_time) * 1000
         target["elapsed_time"] = elapsed_ms
