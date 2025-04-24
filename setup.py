f1ab6414e4bc996aab7e8516c137382f9ab11c65
adamhadani <139888+adamhadani@users.noreply.github.com>
2025-04-23 21:16:57 -0500

    bump min required python to 3.11 to match sonar and drone CI tooling

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=3.10",
+    python_requires=">=3.11",
     keywords="microcosm",
     install_requires=[
         "loggly-python-handler>=1.0.0",
