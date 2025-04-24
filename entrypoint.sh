20dc0753ada745f9b460650a8e7608642801f8bd
adamhadani <139888+adamhadani@users.noreply.github.com>
2025-04-24 06:57:17 -0500

    remove --ignore-missing-imports flag from mypy in entrypoint.sh, not needed here

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,18 +25,16 @@
 
 if [ "$1" = "test" ]; then
    # Install standard test dependencies; YMMV
-   pip --quiet install \
-       .[test] pytest pytest-cov PyHamcrest
+   pip --quiet install .[test] pytest pytest-cov PyHamcrest
    pytest
 elif [ "$1" = "lint" ]; then
    # Install standard linting dependencies; YMMV
-   pip --quiet install \
-       .[lint]
+   pip --quiet install .[lint]
    flake8 ${NAME}
 elif [ "$1" = "typehinting" ]; then
    # Install standard type-linting dependencies
    pip --quiet install mypy
-   exec mypy ${NAME} --ignore-missing-imports
+   mypy ${NAME}
 else
    echo "Cannot execute $@"
    exit 3
